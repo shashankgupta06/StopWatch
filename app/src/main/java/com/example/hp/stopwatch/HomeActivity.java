915bd7aa720d7c9fd588ad65e1c218ad733d099c
shashank gupta <shashank_32@live.com>
2018-04-18 16:27:29 -0400

    A basic Android StopWatch

---
+++ app/src/main/java/com/example/hp/stopwatch/HomeActivity.java
@@ -0,0 +1,45 @@
+package com.example.hp.stopwatch;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText edTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btn = (Button) findViewById(R.id.btn);
+        edTime = (EditText) findViewById(R.id.edTime);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int time = Integer.parseInt(edTime.getText().toString());
+                Intent i = new Intent(HomeActivity.this, StopWatch.class);
+                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(),0,i,0);
+                AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+time*1000,pi);
+
+
+            }
+        });
+    }
+
+}
+
+
+
+
+
+
--- app/src/main/java/com/example/hp/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/hp/stopwatch/MainActivity.java
@@ -1,41 +1,32 @@
 package com.example.hp.stopwatch;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
+
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import java.sql.Time;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 4000;
 
-    Button btn;
-    EditText edTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btn);
-        edTime = (EditText) findViewById(R.id.edTime);
-
-        btn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-            int time = Integer.parseInt(edTime.getText().toString());
-            Intent i = new Intent(MainActivity.this, StopWatch.class);
-            PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(),0,i,0);
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+time*1000,pi);
-
-
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
             }
-        });
+        },SPLASH_TIME_OUT);
     }
+
 }
