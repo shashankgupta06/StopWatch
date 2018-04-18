fcab185e918087e028c8388baedec67dc91206d2
shashank gupta <shashank_32@live.com>
2018-04-18 17:08:35 -0400

    A basic stop watch in android, Splash screen added!

--- app/src/main/java/com/example/hp/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/hp/stopwatch/MainActivity.java
@@ -5,8 +5,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
+import android.view.Window;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(new Runnable() {
