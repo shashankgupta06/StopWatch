a079946cadffe836fafcb0e0ab44b4ff7d4fdcd2
shashank gupta <shashank_32@live.com>
2018-04-20 21:55:11 -0400

    Debugged

--- app/src/main/java/com/example/hp/stopwatch/HomeActivity.java
+++ app/src/main/java/com/example/hp/stopwatch/HomeActivity.java
@@ -1,31 +1,30 @@
 package com.example.hp.stopwatch;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.method.KeyListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+import android.os.CountDownTimer;
 
-import static java.sql.DriverManager.println;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private KeyListener listner;
     public Button btn;
     EditText edTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         btn = (Button) findViewById(R.id.btn);
         edTime = (EditText) findViewById(R.id.edTime);
+        listner = edTime.getKeyListener();
         btn.setEnabled(true);
 
 
@@ -33,23 +32,45 @@ public class HomeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                String text = edTime.getText().toString();
+                final int time = Integer.parseInt(edTime.getText().toString());
 
-                if(edTime.getText().toString().matches("")){
+                if (text.equalsIgnoreCase("")) {
 
-                    Toast.makeText(HomeActivity.this, "Please select a value",Toast.LENGTH_LONG).show();
+                    Toast.makeText(HomeActivity.this, "Please select a value", Toast.LENGTH_LONG).show();
                     return;
                 }
 
-                btn.setEnabled(false);
-                int time = Integer.parseInt(edTime.getText().toString());
-                Intent i = new Intent(HomeActivity.this, StopWatch.class);
-                PendingIntent pi = PendingIntent.getBroadcast(getApplicationContext(),0,i,0);
-                AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-                am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+time*1000,pi);
+                if (!text.equalsIgnoreCase("")) {
+                    edTime.setKeyListener(null);
+                    btn.setEnabled(false);
 
+                    CountDownTimer countDownTimer = new CountDownTimer((time*1000), 1000) {
+
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+
+                            edTime.setText(String.valueOf((int)(millisUntilFinished/1000)));
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            edTime.setText("");
+                            edTime.setKeyListener(listner);
+                            Toast.makeText(HomeActivity.this,"Alarm...",Toast.LENGTH_LONG).show();
+                            Vibrator v = (Vibrator)HomeActivity.this.getSystemService(HomeActivity.this.VIBRATOR_SERVICE);
+                            v.vibrate(2000);
+                            btn.setEnabled(true);
+
+                        }
+                    }.start();
+
+                }
             }
 
         });
+
+
     }
 
 }
